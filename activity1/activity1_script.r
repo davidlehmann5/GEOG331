235887ddcba563e64043549da1d640f5ae90cfad
David Lehmann '22 <dlehmann@colgate.edu>
2020-01-24 12:11:04 -0500

    final r script

--- activity1/activity1_script.r
+++ activity1/activity1_script.r
@@ -5,3 +5,7 @@ print("hello R world")
 10/2 #division
 6 - 2 #subtraction
 8 * 3 #multiplication
+
+a <- 5 #creates a variable
+v <- c(3,4,5,6) # creates a vector
+a * v #r will do calculations with a vector without extra code
